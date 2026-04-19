bd1215c1e862d47d821764c251ff255fa6c0a6ae
Bansi12846 <lathiyabansi2006@gmail.com>
2026-04-19 20:21:05 +0530

    Implement Employee class with data input and output

---
+++ pr7.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class Employee {
+    private String employeeName;
+    private double employeeSalary;
+    public void readEmployeeData() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter name: ");
+        employeeName = sc.nextLine();
+        System.out.print("Enter salary: ");
+        employeeSalary = sc.nextDouble();
+    }
+    public void displayEmployeeData() {
+        System.out.println("Name: " + employeeName);
+        System.out.println("Salary: " + employeeSalary);
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Employee e = new Employee();
+        e.readEmployeeData();
+        e.displayEmployeeData();
+    }
+}
+
+// Input:
+// John
+// 50000
+// Output:
+// Name: John
+// Salary: 50000.0
