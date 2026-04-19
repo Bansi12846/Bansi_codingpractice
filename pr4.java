80a036b254ffc2570c824dde1ef29ffd94dfc5f6
Bansi12846 <lathiyabansi2006@gmail.com>
2026-04-19 20:10:57 +0530

    Add BMI calculation program in pr4.java

---
+++ pr4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void pr4(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter weight in pounds: ");
+        double weight = sc.nextDouble();
+        System.out.print("Enter height in inches: ");
+        double height = sc.nextDouble();
+        double kg = weight * 0.45359237;
+        double meter = height * 0.0254;
+        double bmi = kg / (meter * meter);
+        System.out.println("BMI = " + bmi);
+    }
+}
+
+// Input: 150 65
+// Output: BMI = 24.958579881656807
