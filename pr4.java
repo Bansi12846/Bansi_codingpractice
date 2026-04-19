8de1228b8f4dae5660099968dfc36a015e3752aa
Bansi12846 <lathiyabansi2006@gmail.com>
2026-04-19 22:14:00 +0530

    add BMI calculation

--- pr4.java
+++ pr4.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
-public class Main {
-    public static void pr4(String[] args) {
+public class pr4 {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter weight in pounds: ");
         double weight = sc.nextDouble();
