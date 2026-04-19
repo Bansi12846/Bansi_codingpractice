f6791a9787c0febe777eb0e14e8f4880535543fc
Bansi12846 <lathiyabansi2006@gmail.com>
2026-04-19 22:13:02 +0530

    vowel or not

--- pr3.java
+++ pr3.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main {
+public class pr3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         char ch = sc.next().charAt(0);
