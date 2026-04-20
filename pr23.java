cb718b6ef311abcc1df7345f3bcbb379b3fdd994
Bansi12846 <lathiyabansi2006@gmail.com>
2026-04-20 23:02:04 +0530

    Add Thread1 and Thread2 classes for concurrent execution
    Implement two threads that print numbers in sequence with different intervals.

---
+++ pr23.java
@@ -0,0 +1,43 @@
+class Thread1 extends Thread {
+    public void run() {
+        try {
+            for (int i = 1; i <= 10; i++) {
+                System.out.println("Thread1: " + i);
+                Thread.sleep(1000);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+class Thread2 extends Thread {
+    public void run() {
+        try {
+            for (int i = 11; i <= 20; i++) {
+                System.out.println("Thread2: " + i);
+                Thread.sleep(500);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+public class pr23{
+    public static void main(String[] args) {
+        Thread1 t1 = new Thread1();
+        Thread2 t2 = new Thread2();
+
+        t1.start();
+        t2.start();
+    }
+}
+
+// Output (may vary):
+// Thread1: 1
+// Thread2: 11
+// Thread2: 12
+// Thread1: 2
+// Thread2: 13
+// Thread2: 14
+// Thread1: 3
+// ...
